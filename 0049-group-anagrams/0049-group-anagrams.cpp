91c75fa0f116162edea0e47fc1a4b5d976fb8805
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-06-06 12:29:06 +0530

    Time: 19 ms (45.77%), Space: 29.5 MB (19.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        
-        for(auto str:strs) {
-            string temp = str;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(str);
+string generate(string &word){
+    vector<int> vec(26, 0);
+    for(char &ch: word){
+        vec[ch-'a']++;
+    }
+    string newword = "";
+    for(int i=0 ; i<26; i++){
+        if(vec[i] > 0){
+            newword += string(vec[i], i + 'a');
         }
-        
+    }
+    return newword;
+}
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+
         vector<vector<string>> result;
-        for(auto it : mp) {
+
+        for(int i=0; i<n; i++){
+            string word = strs[i];
+            string newword = generate(word);
+            mp[newword].push_back(word);
+        }
+        for(auto &it: mp){
             result.push_back(it.second);
         }
-        
         return result;
     }
 };
