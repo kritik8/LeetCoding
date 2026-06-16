f56a17f775cbe32b3ca636e5c586d2d67fd9f870
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-06-16 20:12:53 +0530

    Time: 8 ms (8.01%), Space: 111.3 MB (41.66%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -5,17 +5,17 @@ public:
         int n = nums.size();
         int i=0;
         while(i < n){
-            long long l =0;
             if(nums[i] == 0){
+                long long l = 1;
                 while(i<n && nums[i]==0){
                     i++;
+                    result += l;
                     l++;
                 }
             }
             else{
                 i++;
             }
-            result += ((l)*(l+1)/2);
         }
         return result;
     }
