d9e3c517365da75af696cf18fd1122e41d0f8a21
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-06-22 21:17:59 +0530

    Time: 178 ms (31.32%), Space: 21.5 MB (28.51%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int ladderLength(string start, string end, vector<string>& bank) {
+        unordered_set<string> st(bank.begin(), bank.end());
+
+        unordered_set<string> visited;
+        queue<string> q;
+
+        q.push(start);
+        visited.insert(start);
+
+        int lvl =0;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                string curr = q.front();
+                q.pop();
+
+                if(curr == end)
+                    return (lvl+1);
+
+                for(char ch: "abcdefghijklmnopqrstuvwxyz"){
+                    for(int i=0; i<curr.length(); i++){
+                        string next = curr;
+                        next[i] = ch;
+
+                        if(visited.find(next) == visited.end() && st.find(next) != st.end()){
+                            visited.insert(next);
+                            q.push(next);
+                        }
+                    }
+                }
+            }
+            lvl++;
+        }
+        return 0;
+    }
+};
