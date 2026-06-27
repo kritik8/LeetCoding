f699d56ecf9db075a783e3f70db7363c8a1d6485
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-06-28 00:29:34 +0530

    Time: 3 ms (46.05%), Space: 8 MB (35.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n-1;
+        int res=0;
+        while(n>1){
+            if(n%2==0){
+                res += n/2;
+                n = n/2;
+            }else{
+                res += (n-1)/2;
+                n = (n-1)/2 + 1;
+            }
+        }
+        return res;
     }
 };
