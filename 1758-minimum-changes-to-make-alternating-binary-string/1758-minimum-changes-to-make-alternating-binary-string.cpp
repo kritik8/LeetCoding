b7878cc1135363430a010a3d1451b62dce76d04e
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-02 20:30:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (49.93%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int minOperations(string s) {
         int n=s.length();
-        int start_0 =0, start_1 = 0;
+        int start_0 =0;
 
         for(int i=0; i<n; i++){
             if(i%2==0){
-                if(s[i]=='0')
-                    start_1++;
-                else
+                if(s[i]=='1')
                     start_0++;
             }else{
-                if(s[i]=='1')
-                    start_1++;
-                else
+                if(s[i]=='0')
                     start_0++;
             }
         }
-    return min(start_0, start_1);
+    return min(start_0, n - start_0);
     }
 };
