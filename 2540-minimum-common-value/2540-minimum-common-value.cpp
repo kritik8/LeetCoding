cca0e62e7c2437de565c737fbc0afae6d887b053
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-07 19:55:33 +0530

    Time: 0 ms (100%), Space: 54.7 MB (25.16%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(nums1.begin(), nums1.end());
-        for(int &num: nums2){
-            if(st.find(num) != st.end()){
-                return num;
-            }
+        int i=0, j=0;
+        int n= nums1.size();
+        int m = nums2.size();
+        while(i<n && j <m){
+            if(nums1[i]==nums2[j])
+                return nums1[i];
+            else if(nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
         }
         return -1;
     }
