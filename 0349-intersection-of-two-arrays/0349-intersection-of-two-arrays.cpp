b1c79a9addc92746c4aefd22a8c38dd1270e8c6b
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-12 13:31:34 +0530

    Time: 0 ms (100%), Space: 13.8 MB (98.44%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     
-    bool binarySearch(vector<int>& nums, int target) {
-        int l = 0, r = nums.size()-1;
-        
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            
-            if(nums[mid] == target) {
-                return true;
-            } else if(nums[mid] < target) {
-                l = mid+1;
-            } else {
-                r = mid-1;
-            }
-        }
-        
-        return false;
-    }
-    
+
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         sort(begin(nums1), end(nums1));
+        sort(begin(nums2), end(nums2));
         
-        unordered_set<int> st;
+        int m = nums1.size();
+        int n = nums2.size();
         
-        for(int &num : nums2) {
-            if(binarySearch(nums1, num)) {
-                st.insert(num);
+        vector<int> result;
+        int i = 0, j = 0;
+        
+        while(i < m && j < n) {
+
+            if(nums1[i] == nums2[j]) {
+                result.push_back(nums1[i]);
+                
+                while(i < m-1 && nums1[i] == nums1[i+1]) {
+                    i++;
+                }
+                while(j < n-1 && nums2[j] == nums2[j+1]) {
+                    j++;
+                }
+                i++;
+                j++;
+                
+            } else if(nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
-        
-        vector<int> result(begin(st), end(st));
         
         return result;
         
