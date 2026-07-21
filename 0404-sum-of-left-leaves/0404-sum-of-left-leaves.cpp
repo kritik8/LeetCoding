d530d113e659dc2f7c8e1e1b68b05b8668e31951
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-21 23:53:45 +0530

    Time: 0 ms (100%), Space: 16 MB (85.06%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -11,25 +11,20 @@
  */
 class Solution {
 public:
-    int sum(TreeNode* curr, TreeNode* parent) {
-        if(!curr)
+    
+    int solve(TreeNode* curr, bool isLeft) {
+        if(!curr) {
             return 0;
-        
-        int left  = sum(curr->left, curr);
-        int right = sum(curr->right, curr);
-        
-        int sum = 0;
-        if(curr->left == NULL && curr->right == NULL) {
-            if(parent && parent->left == curr)
-                sum += curr->val;
         }
         
-        return left+right+sum;
+        if(!curr->left && !curr->right && isLeft) {
+            return curr->val;
+        }
+        
+        return solve(curr->left, true) + solve(curr->right, false);
     }
+    
     int sumOfLeftLeaves(TreeNode* root) {
-        if(!root)
-            return 0;
-        TreeNode* parent = NULL;
-        return sum(root, parent);
+        return solve(root, false);
     }
 };
