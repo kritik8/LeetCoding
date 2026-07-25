5d6d467ef0bcf5d42d160d71c2baa588bab32075
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-25 19:59:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.8%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-int t[38];
-int find(int n){
-    if(n==0)
-            return 0;
-    if(n==1 || n==2)
-            return 1;
-    if(t[n] != -1)
-        return t[n];
-
-    return t[n]=find(n-1)+ find(n-2)+ find(n-3);    
-}
     int tribonacci(int n) {
-        memset(t, -1, sizeof(t));
-                return find(n);
+        int t[38];
+
+        t[0] = 0;
+        t[1] = 1;
+        t[2] = 1;
+        
+        for (int i = 3; i <= 37; i++) {
+            t[i] = t[i-1] + t[i - 2] + t[i - 3];
+        }
+        
+        return t[n];
     }
 };
