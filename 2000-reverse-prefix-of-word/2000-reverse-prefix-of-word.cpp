045dee6728ae7f13362076f2cd851cd570901f50
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-07-26 11:58:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (89.89%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int j = word.find(ch);
-        int i=0;
-        while(i<j){
-            swap(word[i], word[j]);
-            i++; j--;
-        }
+       reverse(word.begin(), word.begin() + word.find(ch) + 1);
         return word;
     }
 };
