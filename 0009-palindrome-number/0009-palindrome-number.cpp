3b914b3a7e2514624d767a18d18ec94f516ffa42
Kritik Jain <150144010+kritik8@users.noreply.github.com>
2026-09-18 23:02:13 +0530

    Time: 0 ms (100%), Space: 8.7 MB (36.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        else{
+            int n = x;
+            long long rev = 0;
+            while(n>0){
+                rev = rev*10 + (n%10);
+                n=n/10;
+            }
+            if(rev == x)
+                return true;
+        }
+        return false;
+    }
+};
